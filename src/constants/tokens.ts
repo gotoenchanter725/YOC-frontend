a7716c9c26f4273fd8a408135ef67b8c8ca7e125
harrypotter0831 <codingwizard0831@gmail.com>
2023-03-01 12:41:19 +0900

    network case, style set

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -12,10 +12,10 @@ export const ETH_TOKENS: Array<tokenInterface> = [
     {
         "chainId": 5,
         "address": "ETH",
-        "name": "Binance Coin",
-        "symbol": "BNB",
+        "name": process.env.NET_WORK == "ETH" ? "Ether" : "Binance Coin",
+        "symbol": process.env.NET_WORK == "ETH" ? "ETH" : "BNB",
         "decimals": 18,
-        "logoURI": "/images/coins/BNB.png"
+        "logoURI": `/images/coins/${process.env.NET_WORK ? "ETH" : "BNB"}.png`
     }
 ]
 
@@ -23,10 +23,10 @@ export const ETH_TEST_TOKENS: Array<tokenInterface> = [
     {
         "chainId": 5,
         "address": "ETH",
-        "name": "Binance Coin",
-        "symbol": "BNB",
+        "name": process.env.NET_WORK == "ETH" ? "Ether" : "Binance Coin",
+        "symbol": process.env.NET_WORK == "ETH" ? "ETH" : "BNB",
         "decimals": 18,
-        "logoURI": "/images/coins/BNB.png"
+        "logoURI": `/images/coins/${process.env.NET_WORK ? "ETH" : "BNB"}.png`
     }, {
         "chainId": 5,
         "address": USDCToken.address,
@@ -43,35 +43,35 @@ export const ETH_TEST_TOKENS: Array<tokenInterface> = [
         "logoURI": "./images/coins/YOC.png"
     }, {
         "chainId": 5,
-        "address": "0xd6e7650D75a73C1d41d3F02289825872419D760B",
+        "address": process.env.YOC1 + '',
         "name": "YOC1",
         "symbol": "YOC1",
         "decimals": 8,
         "logoURI": "./images/coins/YOC.png"
     }, {
         "chainId": 5,
-        "address": "0x573d5466F80CceDd11a3c1416343f5f1bD3EdB81",
+        "address": process.env.YOC2 + '',
         "name": "YOC2",
         "symbol": "YOC2",
         "decimals": 8,
         "logoURI": "./images/coins/YOC.png"
     }, {
         "chainId": 5,
-        "address": "0x503ef8A56AF4e8cdB0Deb6591700AD831b8E5B8c",
+        "address": process.env.YOC3 + '',
         "name": "YOC3",
         "symbol": "YOC3",
         "decimals": 8,
         "logoURI": "./images/coins/YOC.png"
     }, {
         "chainId": 5,
-        "address": "0x0371ffF077Ebe945e66161a3A764BCa633f6F024",
+        "address": process.env.YOC4 + '',
         "name": "YOC4",
         "symbol": "YOC4",
         "decimals": 8,
         "logoURI": "./images/coins/YOC.png"
     }, {
         "chainId": 5,
-        "address": "0xB008e62d77f42643AC3f0fc9Ee7c00bC94D29EcE",
+        "address": process.env.YOC5 + '',
         "name": "YOC5",
         "symbol": "YOC5",
         "decimals": 8,
