59b3fb7c13e40047723f01cfb8fa4761fcbe1de8
codingwizard0831 <creativemoderndev0831@gmail.com>
2023-07-28 10:27:19 +0900

    rpc update as original

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -6,8 +6,8 @@ const ETH_NETWORK = {
     testnet: {
         // RPC_URL: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
         // CHAIN_ID: 5
-        RPC_URL: "https://eth-sepolia.g.alchemy.com/v2/9XxUB2Hodsix6mDB_6uE4U-Ap6tg4c5c",
-        // RPC_URL: "https://rpc.sepolia.org",
+        // RPC_URL: "https://eth-sepolia.g.alchemy.com/v2/9XxUB2Hodsix6mDB_6uE4U-Ap6tg4c5c",
+        RPC_URL: "https://rpc.sepolia.org",
         CHAIN_ID: 11155111
     }
 }
